b87308c1b61482608da6713f4b1fb45398285d84
FernandoHN99 <nandohneto@gmail.com>
2024-05-14 00:13:33 -0300

    Ajuste Criacao de Reficao:
    - TypeORM construtor eh incializado sempre, tomar cuidado em joins

--- backend/src/app/entities/refeicao.ts
+++ backend/src/app/entities/refeicao.ts
@@ -3,6 +3,7 @@ import Usuario from "./usuario";
 import Util from "../../utils/util";
 import AlimentoConsumido from "./alimentoConsumido";
 import { atualizarRefeicaoObject } from "../schemas/refeicao/atualizarRefeicaoSchema";
+import { criarRefeicaoObject } from "../schemas/refeicao/criarRefeicaoSchema";
 
 
 @Entity('refeicao')
@@ -32,13 +33,15 @@ export default class Refeicao extends BaseEntity {
    @JoinColumn({ name: 'numero_refeicao', referencedColumnName: 'numero_refeicao' })
    alimentosConsumidos: AlimentoConsumido[];
 
-   constructor(id_usuario: string, nome_refeicao: string, numero_refeicao: number) {
+   constructor(criarRefeicaoDados: criarRefeicaoObject, numeroRefeicao: number) {
       super();
-         this.id_usuario = id_usuario;
-         this.numero_refeicao = numero_refeicao;
-         this.nome_refeicao = nome_refeicao;
+      if(criarRefeicaoDados && numeroRefeicao){
+         this.id_usuario = criarRefeicaoDados.id_usuario;
+         this.nome_refeicao = criarRefeicaoDados.nome_refeicao;
+         this.numero_refeicao = numeroRefeicao;
          this.ativa = true;
          this.dt_criacao = Util.criarStrData();
+      }
    }
 
    public atualizar(dadosAtualizacao: atualizarRefeicaoObject): void {
