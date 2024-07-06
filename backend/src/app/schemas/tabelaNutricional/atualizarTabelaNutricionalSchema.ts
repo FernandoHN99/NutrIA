f803f56ac2eacaa14bc50e1d4b8cbf6002e48e06
FernandoHN99 <nandohneto@gmail.com>
2024-07-05 21:13:30 -0300

    Retira unidade medida de atualizacao de tabela nutricional

--- backend/src/app/schemas/tabelaNutricional/atualizarTabelaNutricionalSchema.ts
+++ backend/src/app/schemas/tabelaNutricional/atualizarTabelaNutricionalSchema.ts
@@ -2,14 +2,12 @@ import { z } from 'zod';
 import { criarTabelaNutricionalSchema } from './criarTabelaNutricionalSchema';
 
 const atualizarTabelaNutricionalSchema = criarTabelaNutricionalSchema
-   .omit({ id_alimento: true, id_usuario: true})
    .partial()
+   .required({ id_usuario: true })
+   .omit({ id_alimento: true, unidade_medida: true})
    .extend({
       id_tabela_nutricional: z.number()
          .int().min(1, { message: 'Campo Inválido: ID do Alimento Consumido' }),
-
-      id_usuario: z.string()
-         .uuid('Formato Inválido: ID do Usuário'),
    }
 ).refine(data => Object.keys(data).length > 2, {
       message: 'Nenhum dado fornecido para atualização de alimento consumido',
