c68cb91a2ea2cef620b1a5d6865ba9c4bf55430e
FernandoHN99 <nandohneto@gmail.com>
2024-08-26 01:25:48 -0300

    Altera logica MainNavigator

--- frontend/src/navigation/MainStackNavigator.tsx
+++ frontend/src/navigation/MainStackNavigator.tsx
@@ -1,20 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UnauthenticatedNavigator from './UnauthenticatedNavigator';
 import AuthenticatedNavigator from './AuthenticatedNavigator';
+import LoadingScreen from '../components/LoadingScreen';
+import { useAuthToken } from '../utils/useAuthToken';
+
+
+const Stack = createNativeStackNavigator();
 
 const MainStackNavigator = () => {
-   const [usuario, setUsuario] = useState<boolean>(false);
+   const {token, removeToken} = useAuthToken()
+   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(!!token);
+
+   if (isAuthenticated == null) {
+      return <LoadingScreen loadingMessage='Criando a sua conta...'/>
+   }
 
    return (
       <NavigationContainer>
-         {usuario ? (
-            <AuthenticatedNavigator />
-         ) : (
-            <UnauthenticatedNavigator />
-         )}
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {isAuthenticated ? (
+            <Stack.Screen 
+               name="Auth" 
+               component={AuthenticatedNavigator} />
+           ) : (
+              <Stack.Screen 
+                  name="Unauth" 
+                  component={UnauthenticatedNavigator} 
+                  initialParams={{ setIsAuthenticated }} 
+               />
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
-   );
-};
+    );
+  };
+  
 
 export default MainStackNavigator;
