04b9fc9e9cd82ce934d7e0fe2cf84cc8650bdd82
FernandoHN99 <nandohneto@gmail.com>
2024-09-05 12:43:08 -0300

    Implementa ajustes no Scroll, pendencia de scroll infinito

--- frontend/src/components/DiaScroll.tsx
+++ frontend/src/components/DiaScroll.tsx
@@ -3,33 +3,50 @@ import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native
 import theme from '../styles/theme';
 import { getResponsiveSizeWidth, getResponsiveSizeHeight } from '../utils/utils';
 import { criarStrData } from '../utils/utils';
-import { hexToRgba } from '../utils/utils';
+
 const tamanhoLetras: number = getResponsiveSizeHeight(1.6);
 
-const DiaScroll = ({ diaSelecionado, setDiaSelecionado } : {diaSelecionado: string, setDiaSelecionado: Function}) => {
+const gerarDatas = (dataBase: Date = new Date(), qtdeDiasPassados: number, qtdeDiasFuturos: number) => {
+   const datasControl: { [key: string]: any } = {};
 
+   for (let i= -qtdeDiasPassados; i <= qtdeDiasFuturos; i++) {
+      const data = new Date(dataBase);
+      data.setDate(dataBase.getDate() + i);
+
+      let diaSemana = data.toLocaleDateString('pt-BR', { weekday: 'short' });
+      diaSemana = diaSemana.substring(0, 3).toUpperCase();
+      const dataFormatada = data.toISOString().split('T')[0];
+      const [ano, mes, dia] = dataFormatada.split('-');
+
+      datasControl[dataFormatada] = { id: dataFormatada, data: `${dia}.${mes}`, diaSemana };
+   }
+   return datasControl;
+};
+
+
+interface Dia {
+   id: string;
+   data: string;
+   diaSemana: string;
+}
+
+interface DiaScrollProps {
+   diaSelecionado: string;
+   setDiaSelecionado: Function;
+}
+
+const DiaScroll = ({ diaSelecionado, setDiaSelecionado }: DiaScrollProps) => {
+   const [dias, setDias] = useState<Array<Dia>>(Object.values(gerarDatas(new Date(), 30, 30)));
    const flatListRef = useRef<FlatList>(null);
 
-   const gerarListaDatas = (dataBase: Date = new Date()) => {
-      const listaDatas = [];
-
-      for (let i = -30; i <= 30; i++) {
-         const data = new Date(dataBase);
-         data.setDate(dataBase.getDate() + i);
-
-         let diaSemana = data.toLocaleDateString('pt-BR', { weekday: 'short' });
-         diaSemana = diaSemana.substring(0, 3).toUpperCase();
-         const dataFormatada = data.toISOString().split('T')[0];
-         const [ano, mes, dia] = dataFormatada.split('-');
-
-         // console.log(listaDatas)
-         listaDatas.push({ id: dataFormatada, data: `${dia}.${mes}`, diaSemana });
-      }
-      return listaDatas;
-   };
-
+   
+   useEffect(() => {
+      scrollDiaSelecionado(diaSelecionado);
+   }, [diaSelecionado]);
+   
+   
    const scrollDiaSelecionado = (idDiaSelecionado: string) => {
-      const selectedItem = dias.find((item) => item.id === idDiaSelecionado);
+      const selectedItem = dias.find(dia => dia.id === idDiaSelecionado);
       if (flatListRef.current && selectedItem) {
          flatListRef.current.scrollToItem({
             item: selectedItem,
@@ -39,14 +56,17 @@ const DiaScroll = ({ diaSelecionado, setDiaSelecionado } : {diaSelecionado: stri
       }
    };
 
-   const dias = gerarListaDatas();
+
+   //  const carregarMaisDias = () => {
+   //    const menorData = dias[0].id;
+   //    const maiorData = dias[dias.length - 1].id;
+   //    const novosDiasPassados = Object.values(gerarDatas(criarData(-2,0,0,new Date(menorData)), 30, 0));
+   //    const novosDiasFuturos = Object.values(gerarDatas(criarData(2,0,0,new Date(maiorData)), 0, 30));
+   //    setDias(prevDias => [...novosDiasPassados, ...prevDias, ...novosDiasFuturos]);
+   // };
 
 
-   useEffect(() => {
-      scrollDiaSelecionado(diaSelecionado);
-   }, [diaSelecionado]);
-
-   const renderItem = ({ item }: { item: {id: string, data: string, diaSemana: string}} ) => (
+   const renderItem = ({ item }: { item: Dia }) => (
       <TouchableOpacity
          key={item.data}
          style={styles.diaButton}
@@ -61,7 +81,7 @@ const DiaScroll = ({ diaSelecionado, setDiaSelecionado } : {diaSelecionado: stri
                item.id !== diaSelecionado ?
                   styles.diaSemanaText :
                   styles.diaSemanaSelecionadoText
-               }>{item.id === criarStrData() ? 'HOJE' : item.diaSemana}
+            }>{item.id === criarStrData() ? 'HOJE' : item.diaSemana}
             </Text>
             <Text style={
                item.id !== diaSelecionado ?
@@ -76,32 +96,34 @@ const DiaScroll = ({ diaSelecionado, setDiaSelecionado } : {diaSelecionado: stri
 
    return (
       <View style={styles.flatListContainer}>
-         <FlatList
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            data={dias}
-            renderItem={renderItem}
-            keyExtractor={(dia) => dia.id}
-            ref={flatListRef}
-            onScrollToIndexFailed={(info) => {
-               const wait = new Promise((resolve) => setTimeout(resolve, 1));
-               wait.then(() => {
-                 flatListRef.current?.scrollToIndex({ 
-                  index: info.index,
-                  animated: true,             
-                  viewPosition: 0.5,
-                 });
-               });
-             }}
-         />
+            <FlatList
+               horizontal
+               showsHorizontalScrollIndicator={false}
+               data={dias}
+               renderItem={renderItem}
+               keyExtractor={(dia) => dia.id}
+               ref={flatListRef}
+               // onStartReached={() => {carregarMaisDias(true)}}
+               // onEndReached={() => {carregarMaisDias(false)}}
+               // onEndReachedThreshold={0.0}
+               // onStartReachedThreshold={0.0}
+               onScrollToIndexFailed={(info) => {
+                  const wait = new Promise((resolve) => setTimeout(resolve, 1));
+                  wait.then(() => {
+                     flatListRef.current?.scrollToIndex({
+                        index: info.index,
+                        animated: true,
+                        viewPosition: 0.5,
+                     });
+                  });
+               }}
+            />
       </View>
    );
 };
 
 const styles = StyleSheet.create({
    flatListContainer: {
-      // backgroundColor: theme.colors.backgroundColor,
-
       borderBottomWidth: 1,
       borderTopWidth: 1,
       borderColor: theme.colors.color04
@@ -140,7 +162,6 @@ const styles = StyleSheet.create({
       textAlign: 'center',
       fontFamily: 'NotoSans-Bold',
       borderColor: theme.colors.black,
-      // paddingBottom: 5
    },
 });
 
