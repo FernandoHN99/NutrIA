e848ecc5fdfaa17c30ffb9fcf935afd34ed60acd
FernandoHN99 <nandohneto@gmail.com>
2024-11-20 16:13:15 -0300

    Ajusta App Entry

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,20 @@
-import MainStackNavigator from './navigation/MainStackNavigator';
+import MainStackNavigator from './src/navigation/MainStackNavigator';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/react-query';
+import { queryClient } from './src/lib/react-query';
 
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
    const [loaded, error] = useFonts({
-      'NotoSans-Regular': require('../assets/fonts/NotoSans-Regular.ttf'),
-      'NotoSans-Bold': require('../assets/fonts/NotoSans-Bold.ttf'),
-      'NotoSans-SemiBold': require('../assets/fonts/NotoSans-SemiBold.ttf'),
-      'NotoSans-BoldItalic': require('../assets/fonts/NotoSans-BoldItalic.ttf'),
-      'NotoSans-Italic': require('../assets/fonts/NotoSans-Italic.ttf'),
-      'NotoSans-SemiBoldItalic': require('../assets/fonts/NotoSans-SemiBoldItalic.ttf'),
+      'NotoSans-Regular': require('./assets/fonts/NotoSans-Regular.ttf'),
+      'NotoSans-Bold': require('./assets/fonts/NotoSans-Bold.ttf'),
+      'NotoSans-SemiBold': require('./assets/fonts/NotoSans-SemiBold.ttf'),
+      'NotoSans-BoldItalic': require('./assets/fonts/NotoSans-BoldItalic.ttf'),
+      'NotoSans-Italic': require('./assets/fonts/NotoSans-Italic.ttf'),
+      'NotoSans-SemiBoldItalic': require('./assets/fonts/NotoSans-SemiBoldItalic.ttf'),
 
    });
 
